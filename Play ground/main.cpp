1cc24c1376495c4459836afc8aa58986e00c554b
xy <jianxinyu626@gmail.com>
2021-06-15 14:01:55 -0230

    438FindAllAnagramsInAString

--- Play ground/main.cpp
+++ Play ground/main.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
-#include "methods.h"
-#include "nodes.h"
-
-#include <memory>
-#include <new>
-
 using namespace std;
 
-namespace obj_new_delete{
-    class Obj{
-    public:
-        Obj():mCount(0) { cout << "Obj ctor" << endl; }
-        ~Obj() { cout << "~Obj dtor" << endl; }
-
-    private:
-        int mCount;
-    };
-
-    void test_new_obj(){
-        Obj *obj = new Obj();
-    delete obj;
+class A {
+public:
+    A() { cout << "A()" << endl; }
+    A(const A& a) { cout << "copy A()" << endl; }
+    ~A() { cout << "~A()" << endl; }
+    A& operator=(const A& a) {
+        cout << "operator=" << endl;
+        return *this;
     }
-}
-
+};
+class B {
+public:
+    B(A a) { mA = a; }
+private:
+    A mA;
+};
 int main(){
-    obj_new_delete::test_new_obj();
-    return 0;
+    A a;
+    B b(a);
 }
